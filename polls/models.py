a37b6171966ad045528ad8dac4712c8c1db1220a
arjansunar <arjan.gahatrajsunar@gmail.com>
2022-11-15 12:26:38 +0545

    writing test cases

--- polls/models.py
+++ polls/models.py
@@ -9,7 +9,9 @@ class Question(models.Model):
 
     # custom method dependent on instance values 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
 
 
     def __str__(self):
